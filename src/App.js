1f95cdd6ca8132d1a7e96ccffa160c3ade0068a3
Mariano0o92 <mariuszn13@gmail.com>
2024-01-15 13:00:25 +0100

    Code refactoring.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Text from './components/Text.js'
+import Fiora from './images/Fiora.png'
+import Fiorkathedoggo from './images/Fiorkathedoggo.png'
+import Full from './images/Full.png'
+import Mountains from './images/Mountains.png'
 
 function App() {
 	return (
-		<Parallax pages={2} className='animation'>
-			<ParallaxLayer offset={0} speed={0.2}>
-				<div className='animationLayer' id='background'></div>
-			</ParallaxLayer>
-			<ParallaxLayer offset={0} speed={0.35}>
-				<div className='animationLayer' id='dog'></div>
-			</ParallaxLayer>
-			<ParallaxLayer offset={0} speed={-0.3}>
-				<div className='animationLayer' id='title'></div>
-			</ParallaxLayer>
-			<ParallaxLayer offset={0} speed={0.35}>
-				<div className='animationLayer' id='mountains'></div>
-			</ParallaxLayer>
-			<ParallaxLayer offset={1} speed={0.35}>
-				<Text />
-			</ParallaxLayer>
-		</Parallax>
+		<div className="wrapper">
+<header>
+			<img src={Full} alt='' className='background' />
+
+			<img src={Fiora} alt='' className='dog' />
+
+			<img src={Fiorkathedoggo} alt='' className='title' />
+
+			<img src={Mountains} alt='' className='mountains' />
+</header>
+
+<Text />
+		</div>
+
 	)
 }
 
